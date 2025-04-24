ea30e5aee610490364ec33beeb49a5bfc3834722
Hari Chirukuri <Hari.Chirukuri@INFOVISIONLABS.COM>
2025-04-24 14:51:31 +0530

    mock

---
+++ mock/src/main/java/com/example/mock/MockApplication.java
@@ -0,0 +1,21 @@
+package com.example.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockApplication.class, args);
+		System.out.println("mock class started");
+	}
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+}
